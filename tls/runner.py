b0d1429427e98adce82257c4cdf19c12828d3357
Ishan Maheshwari <ishoo1997@gmail.com>
2018-06-11 14:31:57 +0530

    TraCI debugged

--- tls/runner.py
+++ tls/runner.py
@@ -45,17 +45,41 @@ def generate_routefile():
     pWE = 1. / 10
     pEW = 1. / 11
     pNS = 1. / 30
-    with open("data/cross.rou.xml", "w") as routes:
+    with open("data/cross_auto.rou.xml", "w") as routes:
         print("""<routes>
-        <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" guiShape="passenger"/>
-        <vType id="typeNS" accel="0.8" decel="4.5" sigma="0.5" length="7" minGap="3" maxSpeed="25" guiShape="bus"/>
+    <vTypeDistribution id="mixed">
+        <vType id="car" vClass="passenger" speedDev="0.2" latAlignment="compact" probability="0.3"/>
+        <vType id="moped" vClass="moped" speedDev="0.4" latAlignment="compact" probability="0.7"/>
+    </vTypeDistribution>
 
-        <route id="right" edges="51o 1i 2o 52i" />
-        <route id="left" edges="52o 2i 1o 51i" />
-        <route id="down" edges="54o 4i 3o 53i" />""", file=routes)
+    <route id="r0" edges="51o 1i 2o 52i"/>
+    <route id="r1" edges="51o 1i 4o 54i"/>
+    <route id="r2" edges="51o 1i 3o 53i"/>
+    <route id="r3" edges="54o 4i 3o 53i"/>
+    <route id="r4" edges="54o 4i 1o 51i"/>
+    <route id="r5" edges="54o 4i 2o 52i"/>
+    <route id="r6" edges="52o 2i 1o 51i"/>
+    <route id="r7" edges="52o 2i 4o 54i"/>
+    <route id="r8" edges="52o 2i 3o 53i"/>
+    <route id="r9" edges="53o 3i 4o 54i"/>
+    <route id="r10" edges="53o 3i 1o 51i"/>
+    <route id="r11" edges="53o 3i 2o 52i"/>
+    <flow id="mixed1" begin="0" end="1500" number="100" route="r0" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed2" begin="0" end="1500" number="100" route="r1" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed3" begin="0" end="1500" number="100" route="r2" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed4" begin="0" end="1500" number="100" route="r3" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed5" begin="0" end="1500" number="100" route="r4" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed6" begin="0" end="1500" number="100" route="r5" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed7" begin="0" end="1500" number="100" route="r6" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed8" begin="0" end="1500" number="100" route="r7" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed9" begin="0" end="1500" number="100" route="r8" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed10" begin="0" end="1500" number="100" route="r9" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed11" begin="0" end="1500" number="100" route="r10" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed12" begin="0" end="1500" number="100" route="r11" type="mixed" departLane="random" departPosLat="random"/>
+</routes>""", file=routes)
         lastVeh = 0
         vehNr = 0
-        for i in range(N):
+    ''' for i in range(N):
             if random.uniform(0, 1) < pWE:
                 print('    <vehicle id="right_%i" type="typeWE" route="right" depart="%i" />' % (
                     vehNr, i), file=routes)
@@ -71,7 +95,8 @@ def generate_routefile():
                     vehNr, i), file=routes)
                 vehNr += 1
                 lastVeh = i
-        print("</routes>", file=routes)
+        print("</routes>", file=routes)'''
+    
 
 # The program looks like this
 #    <tlLogic id="0" type="static" programID="0" offset="0">
@@ -87,18 +112,19 @@ def run():
     """execute the TraCI control loop"""
     step = 0
     # we start with phase 2 where EW has green
-    traci.trafficlight.setPhase("0", 2)
+    traci.trafficlight.setPhase("0", 0)
     while traci.simulation.getMinExpectedNumber() > 0:
-        time.sleep(0.01)
+        
         traci.simulationStep()
-        if traci.trafficlight.getPhase("0") == 2:
+        
+        '''if traci.trafficlight.getPhase("0") == 2:
             # we are not already switching
             if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
                 # there is a vehicle from the north, switch
                 traci.trafficlight.setPhase("0", 3)
             else:
                 # otherwise try to keep green for EW
-                traci.trafficlight.setPhase("0", 2)
+                traci.trafficlight.setPhase("0", 2)'''
         step += 1
     traci.close()
     sys.stdout.flush()
