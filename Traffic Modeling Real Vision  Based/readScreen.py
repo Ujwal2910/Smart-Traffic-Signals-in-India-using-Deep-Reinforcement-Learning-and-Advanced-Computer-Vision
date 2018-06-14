dde4d5959a92a253337e4e42262e3d4b49095ab8
Ujwal Tewari <ujwalkaka@gmail.com>
2018-06-14 16:15:44 +0530

    added tail length changes

--- Traffic Modeling Real Vision  Based/readScreen.py
+++ Traffic Modeling Real Vision  Based/readScreen.py
@@ -7,48 +7,95 @@ mon = {'top': 140, 'left': 100, 'width': 884, 'height': 900}
 
 sct = mss()
 
-fgbg = cv2.createBackgroundSubtractorMOG2()
+fgbg_upper = cv2.createBackgroundSubtractorMOG2(history=5000000)
+fgbg_lower = cv2.createBackgroundSubtractorMOG2(history=5000000)
+fgbg_right = cv2.createBackgroundSubtractorMOG2(history=5000000)
+fgbg_left = cv2.createBackgroundSubtractorMOG2(history=5000000)
 
-checker  = np.zeros((80),dtype=int)
 
 
 
-def warped_simulation(rect,frame):
+def warped_simulation(rect, frame):
     dst = np.array([(125, 0), (375, 0), (500, 800), (0, 800)], dtype="float32")
 
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(frame, M, (500, 800))
-    #cv2.imshow('warped', warped)
+    # cv2.imshow('warped', warped)
     return warped
 
+'''
+def makeMask(lane, SIDE):
+    if SIDE == 'upper':
+        fgmask = fgbg_upper.apply(lane)
+        #cv2.imshow("bog_upper", fgmask)
+    elif SIDE == 'lower':
+        fgmask = fgbg_lower.apply(lane)
+        #cv2.imshow("bog_lower", fgmask)
+    elif SIDE == 'right':
+        fgmask = fgbg_right.apply(lane)
+        #cv2.imshow("bog_right", fgmask)
+    elif SIDE == 'left':
+        fgmask = fgbg_left.apply(lane)
+        #cv2.imshow("bog_left", fgmask)
 
-def tail_length(lane):
+    return fgmask
+    
+'''
 
-    fgmask = fgbg.apply(lane)
-    #cv2.imshow("bog",fgmask)
+def subtractImage(image,SIDE):
+
+    if SIDE == 'upper':
+        bg = cv2.imread('upper_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow("sub_upper", fgmask)
+
+    elif SIDE == 'lower':
+        bg = cv2.imread('lower_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow("bog_lower", fgmask)
+
+    elif SIDE == 'right':
+        bg = cv2.imread('right_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow("bog_right", fgmask)
+
+    elif SIDE == 'left':
+        bg = cv2.imread('left_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow("bog_left", fgmask)
+
+
+    return fgmask
+
+
+
+
+def tail_length(mask):
+    checker = np.zeros((80), dtype=int)
     start = 800
 
     for i in range(80):
-        density = lane[ start-10:start ,62:438]
-        cv2.imshow("density",density)
-        d_gray = cv2.cvtColor(density, cv2.COLOR_BGR2GRAY)
-        white = cv2.countNonZero(d_gray)
+        density = mask[start-10:start, 0:500]
+
+        white = cv2.countNonZero(density)
         print(" ", white)
-        #start +=10
+        # start +=10
 
-
-        if white > 1500:
+        if white > 1000:
             checker[i] = 1
         else:
             checker[i] = 0
         start -= 10
 
+    tail = 80
 
-    tail = 800
-
-    for i in range(77):
+    for i in range(65):
         over = 1
-        for j in range(i, i + 3):
+        for j in range(i, i + 15):
             if checker[j] == 1:
                 over = 0
                 break
@@ -59,6 +106,9 @@ def tail_length(lane):
 
     print(checker)
     print(tail)
+
+    if tail<5:
+        tail = 0
 
     return tail
 
@@ -80,59 +130,66 @@ while 1:
     M270 = cv2.getRotationMatrix2D(center, 270, 1.0)
     rotated270 = cv2.warpAffine(image, M270, (w, h))
 
-    upper_lane =  np.array([(428,39),(517,39),(517,351),(428,351)], dtype = "float32")
+    upper_lane = np.array([(428, 39), (517, 39), (517, 351), (428, 351)], dtype="float32")
 
     # coordinates on 180 rotation image
-    lower_lane =  np.array([(456,31),(544,31),(544,349),(456,349)], dtype = "float32")
+    lower_lane = np.array([(456, 31), (544, 31), (544, 349), (456, 349)], dtype="float32")
 
-    #coordinates on 90 rotation image
-    right_lane =  np.array([(445,36),(533,36),(533,363),(445,363)], dtype = "float32")
+    # coordinates on 90 rotation image
+    right_lane = np.array([(445, 36), (533, 36), (533, 363), (445, 363)], dtype="float32")
 
-    #cordinates on 270 rotation image
-    left_lane  =  np.array([(439,34),(528,34),(528,334),(439,334)], dtype = "float32")
+    # cordinates on 270 rotation image
+    left_lane = np.array([(439, 34), (528, 34), (528, 334), (439, 334)], dtype="float32")
 
 
 
-    warp_upperlane = warped_simulation(upper_lane,image)
-    cv2.imshow("upper",warp_upperlane)
+
+
+    warp_upperlane = warped_simulation(upper_lane, image)
+    # cv2.imshow("upper", warp_upperlane)
 
     warp_lowerlane = warped_simulation(lower_lane, rotated180)
-    #cv2.imshow("lower", warp_lowerlane)
+    # cv2.imshow("lower", warp_lowerlane)
 
     warp_rightlane = warped_simulation(right_lane, rotated90)
-    #cv2.imshow("right", warp_rightlane)
+    # cv2.imshow("right", warp_rightlane)
 
     warp_leftlane = warped_simulation(left_lane, rotated270)
-    #cv2.imshow("left", warp_leftlane)
+    # cv2.imshow("left", warp_leftlane)
 
-    tail_length_upper = tail_length(warp_upperlane)
-    tail_length_lower = tail_length(warp_lowerlane)
-    tail_length_right = tail_length(warp_rightlane)
-    tail_length_left  = tail_length(warp_leftlane)
+
+
+
+
+
+    mask_upper = subtractImage(warp_upperlane, "upper")
+    mask_lower = subtractImage(warp_lowerlane, "lower")
+    mask_right = subtractImage(warp_rightlane, "right")
+    mask_left = subtractImage(warp_leftlane, "left")
+
+
+    tail_length_upper = tail_length(mask_upper)
+    tail_length_lower = tail_length(mask_lower)
+    tail_length_right = tail_length(mask_right)
+    tail_length_left = tail_length(mask_left)
 
     print("Tail lengths - ")
 
-    print("Upper Tail ",tail_length_upper)
+    print("Upper Tail ", tail_length_upper)
     print("lower Tail ", tail_length_lower)
     print("right Tail ", tail_length_right)
     print("left Tail ", tail_length_left)
 
     print("  end")
 
+    # cv2.imshow('test', image)
+    # cv2.imshow("180",rotated180)
+    # cv2.imshow("90",rotated90)
+    # cv2.imshow("270",rotated270)
 
-
-
-    #cv2.imshow('test', image)
-    #cv2.imshow("180",rotated180)
-    #cv2.imshow("90",rotated90)
-    #cv2.imshow("270",rotated270)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
-
-
-
-
 
 '''
 def tail_length(frame):
