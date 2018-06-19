979f60146525ca319c3eba7d6c8d7162e4bb8ff3
Ishan Maheshwari <ishoo1997@gmail.com>
2018-06-19 22:30:40 +0530

    updated agent and readScreen

--- Traffic Modeling Real Vision  Based/Agent_.py
+++ Traffic Modeling Real Vision  Based/Agent_.py
@@ -1,5 +1,106 @@
-from readScreen import State_Lengths
-from runner import Runner
+from __future__ import absolute_import
+from __future__ import print_function
+from select import select
+import termios
+import os
+import sys
+import optparse
+import subprocess
+import random
+import time
+import cv2
+import curses
+
+import readScreen2
+import numpy as np
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+def generate_routefile():
+    random.seed(42)  # make tests reproducible
+    N = 3600  # number of time steps
+    # demand per second from different directions
+    pWE = 1. / 10
+    pEW = 1. / 11
+    pNS = 1. / 30
+    with open("data/cross_auto.rou.xml", "w") as routes:
+        print("""<routes>
+    <vTypeDistribution id="mixed">
+        <vType id="car" vClass="passenger" speedDev="0.2" latAlignment="compact" probability="0.3"/>
+        <vType id="moped" vClass="moped" speedDev="0.4" latAlignment="compact" probability="0.7"/>
+    </vTypeDistribution>
+
+    <route id="r0" edges="51o 1i 2o 52i"/>
+    <route id="r1" edges="51o 1i 4o 54i"/>
+    <route id="r2" edges="51o 1i 3o 53i"/>
+    <route id="r3" edges="54o 4i 3o 53i"/>
+    <route id="r4" edges="54o 4i 1o 51i"/>
+    <route id="r5" edges="54o 4i 2o 52i"/>
+    <route id="r6" edges="52o 2i 1o 51i"/>
+    <route id="r7" edges="52o 2i 4o 54i"/>
+    <route id="r8" edges="52o 2i 3o 53i"/>
+    <route id="r9" edges="53o 3i 4o 54i"/>
+    <route id="r10" edges="53o 3i 1o 51i"/>
+    <route id="r11" edges="53o 3i 2o 52i"/>
+
+    <flow id="mixed1" begin="0" end="15000" number="500" route="r0" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed2" begin="0" end="15000" number="500" route="r1" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed3" begin="0" end="15000" number="500" route="r2" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed4" begin="0" end="15000" number="500" route="r3" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed5" begin="0" end="15000" number="500" route="r4" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed6" begin="0" end="15000" number="500" route="r5" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed7" begin="0" end="15000" number="500" route="r6" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed8" begin="0" end="15000" number="500" route="r7" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed9" begin="0" end="15000" number="500" route="r8" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed10" begin="0" end="15000" number="500" route="r9" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed11" begin="0" end="15000" number="500" route="r10" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed12" begin="0" end="15000" number="500" route="r11" type="mixed" departLane="random" departPosLat="random"/>
+</routes>""", file=routes)
+        lastVeh = 0
+        vehNr = 0
+
+
+
+try:
+    sys.path.append(os.path.join(os.path.dirname(
+        __file__), '..', '..', '..', '..', "tools"))  # tutorial in tests
+    sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(
+        os.path.dirname(__file__), "..", "..", "..")), "tools"))  # tutorial in docs
+    from sumolib import checkBinary  # noqa
+except ImportError:
+    sys.exit(
+        "please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin', 'tools' and 'docs')")
+
+import traci
+
+options = get_options()
+
+# this script has been called from the command line. It will start sumo as a
+# server, then connect and run
+
+if options.nogui:
+    sumoBinary = checkBinary('sumo')
+else:
+    sumoBinary = checkBinary('sumo-gui')
+
+# first, generate the route file for this simulation
+generate_routefile()
+print("Now starting TraCI")
+# this is the normal way of using traci. sumo is started as a
+# subprocess and then the python script connects and runs
+traci.start([sumoBinary, "-c", "data/cross.sumocfg",
+                         "--tripinfo-output", "tripinfo.xml"])
+
+print("TraCI Started")
+
+
+
 
 #State = State_Lengths()
 #print(State.get_tails())
@@ -7,4 +108,210 @@ from runner import Runner
 #states = State.get_tails
 
 
-print(Runner().run)
+
+#runner = Runner()
+#print(Runner().run)
+
+
+def getState():
+
+    #print(States_.get_tails())
+
+    state = np.zeros((5,1))
+
+    state[0,0] = readScreen2.getUpperQlength()
+    state[1,0] = readScreen2.getLowerQlength()
+    state[2,0] = readScreen2.getRightQlength()
+    state[3,0] = readScreen2.getLeftQlength()
+    phase = traci.trafficlight.getPhase("0")
+    state[4,0] = phase
+    print (state)
+
+    return state
+
+
+
+print("here")
+import traci
+
+
+
+def makeMove(state,action):
+    if action == 1:
+        traci.trafficlight.setPhase("0",((traci.trafficlight.getPhase("0") + 1)%8))
+
+    # agent.simulateFrames(SIM_FRAMES)
+    traci.simulationStep()
+    traci.simulationStep()
+    traci.simulationStep()
+    traci.simulationStep()
+    traci.simulationStep()
+    traci.simulationStep()
+    traci.simulationStep()
+    traci.simulationStep()
+    traci.simulationStep()
+
+    newState = getState()
+
+    return newState
+
+def getReward(state):
+    qLengths = state[:4]
+    reward = (-1) * np.average(qLengths) * np.std(qLengths)
+
+    return reward
+
+
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import RMSprop
+
+model = Sequential()
+model.add(Dense(164, kernel_initializer='lecun_uniform', input_shape=(5,)))
+model.add(Activation('relu'))
+#model.add(Dropout(0.2)) I'm not using dropout, but maybe you wanna give it a try?
+
+model.add(Dense(150, kernel_initializer='lecun_uniform'))
+model.add(Activation('relu'))
+#model.add(Dropout(0.2))
+
+model.add(Dense(2, kernel_initializer='lecun_uniform'))
+model.add(Activation('linear')) #linear output so we can have range of real-valued outputs
+
+rms = RMSprop()
+model.compile(loss='mse', optimizer=rms)
+#model.predict(state.reshape(1,5), batch_size=1)
+
+model.compile(loss='mse', optimizer=rms)#reset weights of neural network
+epochs = 100
+gamma = 0.975
+epsilon = 1
+batchSize = 40
+buffer = 80
+replay = []
+#stores tuples of (S, A, R, S')
+h = 0
+for i in range(epochs):
+
+    """execute the TraCI control loop"""
+
+
+    traci.trafficlight.setPhase("0", 0)
+    state = getState()
+    counter = 1
+
+    while traci.simulation.getMinExpectedNumber() > 0:
+
+        qval = model.predict(state.reshape(1,5), batch_size=1)
+        if (random.random() < epsilon): #choose random action
+            action = np.random.randint(0,2)
+        else: #choose best action from Q(s,a) values
+            action = (np.argmax(qval))
+        #Take action, observe new state S'
+        new_state = makeMove(state, action)
+
+        #Observe reward
+        reward = getReward(new_state)
+
+        #Experience replay storage
+        if (len(replay) < buffer): #if buffer not filled, add to it
+            replay.append((state, action, reward, new_state))
+        else: #if buffer full, overwrite old values
+            if (h < (buffer-1)):
+                h += 1
+            else:
+                h = 0
+            replay[h] = (state, action, reward, new_state)
+            #randomly sample our experience replay memory
+            minibatch = random.sample(replay, batchSize)
+            X_train = []
+            y_train = []
+            for memory in minibatch:
+                #Get max_Q(S',a)
+                old_state, action, reward, new_state = memory
+                old_qval = model.predict(old_state.reshape(1,5), batch_size=1)
+                newQ = model.predict(new_state.reshape(1,5), batch_size=1)
+                maxQ = np.max(newQ)
+                y = np.zeros((1,2))
+                y[:] = old_qval[:]
+                update = (reward + (gamma * maxQ))
+                y[0][action] = update
+                X_train.append(old_state.reshape(5,))
+                y_train.append(y.reshape(2,))
+
+            X_train = np.array(X_train)
+            y_train = np.array(y_train)
+            print("Frame #: %s" % (i,))
+            model.fit(X_train, y_train, batch_size=batchSize, nb_epoch=1, verbose=1)
+            state = new_state
+        #status += 1
+        if epsilon > 0.1: #decrement epsilon over time
+            epsilon -= (1/1000)
+        counter +=1
+
+
+
+
+
+        getState()
+
+    #.curses.endwin()
+
+    traci.close()
+    sys.stdout.flush()
+
+'''
+    state = getState() #using the harder state initialization function
+    status = 1
+    #while game still in progress
+    while(status < 10000):
+        #We are in state S
+        #Let's run our Q function on S to get Q values for all possible actions
+        qval = model.predict(state.reshape(1,5), batch_size=1)
+        if (random.random() < epsilon): #choose random action
+            action = np.random.randint(0,2)
+        else: #choose best action from Q(s,a) values
+            action = (np.argmax(qval))
+        #Take action, observe new state S'
+        new_state = makeMove(state, action)
+        #Observe reward
+        reward = getReward(new_state)
+
+        #Experience replay storage
+        if (len(replay) < buffer): #if buffer not filled, add to it
+            replay.append((state, action, reward, new_state))
+        else: #if buffer full, overwrite old values
+            if (h < (buffer-1)):
+                h += 1
+            else:
+                h = 0
+            replay[h] = (state, action, reward, new_state)
+            #randomly sample our experience replay memory
+            minibatch = random.sample(replay, batchSize)
+            X_train = []
+            y_train = []
+            for memory in minibatch:
+                #Get max_Q(S',a)
+                old_state, action, reward, new_state = memory
+                old_qval = model.predict(old_state.reshape(1,5), batch_size=1)
+                newQ = model.predict(new_state.reshape(1,5), batch_size=1)
+                maxQ = np.max(newQ)
+                y = np.zeros((1,2))
+                y[:] = old_qval[:]
+                update = (reward + (gamma * maxQ))
+                y[0][action] = update
+                X_train.append(old_state.reshape(5,))
+                y_train.append(y.reshape(2,))
+
+            X_train = np.array(X_train)
+            y_train = np.array(y_train)
+            print("Frame #: %s" % (i,))
+            model.fit(X_train, y_train, batch_size=batchSize, nb_epoch=1, verbose=1)
+            state = new_state
+        #status += 1
+        clear_output(wait=True)
+    if epsilon > 0.1: #decrement epsilon over time
+        epsilon -= (1/epochs)
+
+'''
---
+++ Traffic Modeling Real Vision  Based/readScreen2.py
@@ -0,0 +1,205 @@
+import numpy as np
+import cv2
+from mss import mss
+from PIL import Image
+
+mon = {'top': 140, 'left': 100, 'width': 884, 'height': 900}
+
+sct = mss()
+
+fgbg_upper = cv2.createBackgroundSubtractorMOG2(history=5000000)
+fgbg_lower = cv2.createBackgroundSubtractorMOG2(history=5000000)
+fgbg_right = cv2.createBackgroundSubtractorMOG2(history=5000000)
+fgbg_left = cv2.createBackgroundSubtractorMOG2(history=5000000)
+
+
+def warped_simulation(rect, frame):
+    dst = np.array([(125, 0), (375, 0), (500, 800), (0, 800)], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(frame, M, (500, 800))
+    # cv2.imshow('warped', warped)
+    return warped
+
+
+'''
+def makeMask(lane, SIDE):
+    if SIDE == 'upper':
+        fgmask = fgbg_upper.apply(lane)
+        #cv2.imshow("bog_upper", fgmask)
+    elif SIDE == 'lower':
+        fgmask = fgbg_lower.apply(lane)
+        #cv2.imshow("bog_lower", fgmask)
+    elif SIDE == 'right':
+        fgmask = fgbg_right.apply(lane)
+        #cv2.imshow("bog_right", fgmask)
+    elif SIDE == 'left':
+        fgmask = fgbg_left.apply(lane)
+        #cv2.imshow("bog_left", fgmask)
+
+    return fgmask
+
+'''
+
+
+def subtractImage(image, SIDE):
+    if SIDE == 'upper':
+        bg = cv2.imread('upper_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("sub_upper", fgmask)
+
+    elif SIDE == 'lower':
+        bg = cv2.imread('lower_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("bog_lower", fgmask)
+
+    elif SIDE == 'right':
+        bg = cv2.imread('right_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("bog_right", fgmask)
+
+    elif SIDE == 'left':
+        bg = cv2.imread('left_background.png')
+        fgmask = image - bg
+        fgmask = cv2.cvtColor(fgmask, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow("bog_left", fgmask)
+
+    return fgmask
+
+
+def tail_length(mask):
+    checker = np.zeros((80), dtype=int)
+    start = 800
+
+    for i in range(80):
+        density = mask[start - 10:start, 0:500]
+
+        white = cv2.countNonZero(density)
+        #print(" ", white)
+        # start +=10
+
+        if white > 1000:
+            checker[i] = 1
+        else:
+            checker[i] = 0
+        start -= 10
+
+    tail = 80
+
+    for i in range(65):
+        over = 1
+        for j in range(i, i + 15):
+            if checker[j] == 1:
+                over = 0
+                break
+
+        if over == 1:
+            tail = i
+            break
+
+    #print(checker)
+    #print(tail)
+
+    if tail < 5:
+        tail = 0
+
+    return tail
+
+
+def getScreenImage():
+    sct.get_pixels(mon)
+    img = Image.frombytes('RGB', (sct.width, sct.height), sct.image)
+    image = np.array(img)
+
+    return image
+
+
+def getCenter(image):
+    (h, w) = image.shape[:2]
+    center = (w / 2, h / 2)
+
+    return h, w, center
+
+
+def getUpperQlength():
+    image = getScreenImage()
+    upper_lane = np.array([(428, 39), (517, 39), (517, 351), (428, 351)], dtype="float32")
+    warp_upperlane = warped_simulation(upper_lane, image)
+    mask_upper = subtractImage(warp_upperlane, "upper")
+    tail_length_upper = tail_length(mask_upper)
+
+    return tail_length_upper
+
+
+def getLowerQlength():
+    image = getScreenImage()
+    h, w, center = getCenter(image)
+    M180 = cv2.getRotationMatrix2D(center, 180, 1.0)
+    rotated180 = cv2.warpAffine(image, M180, (w, h))
+    lower_lane = np.array([(456, 31), (544, 31), (544, 349), (456, 349)], dtype="float32")
+    warp_lowerlane = warped_simulation(lower_lane, rotated180)
+    mask_lower = subtractImage(warp_lowerlane, "lower")
+    tail_length_lower = tail_length(mask_lower)
+
+    return tail_length_lower
+
+
+def getRightQlength():
+    image = getScreenImage()
+    h, w, center = getCenter(image)
+    M90 = cv2.getRotationMatrix2D(center, 90, 1.0)
+    rotated90 = cv2.warpAffine(image, M90, (w, h))
+    right_lane = np.array([(445, 36), (533, 36), (533, 363), (445, 363)], dtype="float32")
+    warp_rightlane = warped_simulation(right_lane, rotated90)
+    mask_right = subtractImage(warp_rightlane, "right")
+    tail_length_right = tail_length(mask_right)
+
+    return tail_length_right
+
+
+def getLeftQlength():
+    image = getScreenImage()
+    h, w, center = getCenter(image)
+    M270 = cv2.getRotationMatrix2D(center, 270, 1.0)
+    rotated270 = cv2.warpAffine(image, M270, (w, h))
+    left_lane = np.array([(439, 34), (528, 34), (528, 334), (439, 334)], dtype="float32")
+    warp_leftlane = warped_simulation(left_lane, rotated270)
+    mask_left = subtractImage(warp_leftlane, "left")
+    tail_length_left = tail_length(mask_left)
+    return tail_length_left
+
+
+if __name__ == '__main__':
+    while 1:
+
+        tail_length_upper = getUpperQlength()
+        tail_length_lower = getLowerQlength()
+        tail_length_right = getRightQlength()
+        tail_length_left = getLeftQlength()
+
+        # print("Tail lengths - ")
+        #
+        # print("Upper Tail ", tail_length_upper)
+        # print("lower Tail ", tail_length_lower)
+        # print("right Tail ", tail_length_right)
+        # print("left Tail ", tail_length_left)
+        #
+        # print("  end")
+
+        # cv2.imshow('test', image)
+        # cv2.imshow("180",rotated180)
+        # cv2.imshow("90",rotated90)
+        # cv2.imshow("270",rotated270)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+    '''
+    def tail_length(frame):
+
+        return tail_length
+    '''
