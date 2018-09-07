Corrected state representation and reward calculation

Traffic Modeling Real Vision  Based/new.py
@@ -108,7 +108,7 @@ def generate_routefile():
     <flow id="mixed1" begin="0" end="350" number="10" route="r0" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed2" begin="0" end="0" number="0" route="r1" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed3" begin="0" end="0" number="0" route="r2" type="mixed" departLane="random" departPosLat="random"/>
-    <flow id="mixed4" begin="0" end="0" number="90" route="r3" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed4" begin="0" end="350" number="90" route="r3" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed5" begin="0" end="0" number="0" route="r4" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed6" begin="0" end="0" number="0" route="r5" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed7" begin="0" end="0" number="0" route="r6" type="mixed" departLane="random" departPosLat="random"/>
@@ -162,9 +162,13 @@ print("TraCI Started")
 
 
 def getPhaseState(transition_time):
+    num_lanes = 4
+    num_phases = 4
     phase = traci.trafficlight.getPhase("0")
-    phaseState = np.zeros((4,transition_time,4))
-    phaseState[phase] = np.ones((transition_time,4))
+    phaseState = np.zeros((transition_time,num_lanes,num_phases))
+    for i in range(transition_time):
+        for j in range(num_lanes):
+            phaseState[i][j][phase] = 1
     return phaseState
 
 
@@ -183,8 +187,8 @@ def getState(transition_time):  # made the order changes
     # print (state)
     newState = np.array(newState)
     phaseState = getPhaseState(transition_time)
-    newState = np.append(newState, phaseState)
-    return np.array(newState).reshape(1,transition_time,4,5)
+    newState = np.dstack((newState, phaseState))
+    return newState
 
 
 print("here")
@@ -207,8 +211,12 @@ def makeMove(action, transition_time):
 
 
 def getReward(this_state, this_new_state):
-    qLengths1 = this_state[0]
-    qLengths2 = this_new_state[0]
+    num_lanes = 4
+    qLengths1 = []
+    qLengths2 = []
+    for i in range(num_lanes):
+        qLengths1.append(this_state[0][i][0])
+        qLengths2.append(this_new_state[0][i][0])
 
     qLengths11 = [x + 1 for x in qLengths1]
     qLengths21 = [x + 1 for x in qLengths2]
@@ -258,12 +266,12 @@ def getWaitingTime(laneID):
     return traci.lane.getWaitingTime(laneID)
 
 
-num_episode = 101
+num_episode = 61
 discount_factor = 0.9
 #epsilon = 1
 epsilon_start = 1
 epsilon_end = 0.01
-epsilon_decay_steps = 10000
+epsilon_decay_steps = 4000
 
 Average_Q_lengths = []
 sum_q_lens = 0
@@ -401,8 +409,8 @@ for episode in range(num_episode):
 
     AVG_Q_len_perepisode.append(sum_q_lens / 702)
     sum_q_lens = 0
-    if episode % 25 == 0:
-        q_estimator_model.save('new_model_2008_1_{}.h5'.format(episode))
+    if episode % 5 == 0:
+        q_estimator_model.save('new_model_0709_3_{}.h5'.format(episode))
 
 
 

Traffic Modeling Real Vision  Based/readscreen3.py
@@ -75,7 +75,7 @@ def subtractImage(image, SIDE):
 def tail_length(mask):
     checker = np.zeros((80), dtype=int)
     start = 800
-    width_threshold = 300
+    width_threshold = 200
     for i in range(80):
         density = mask[start - 10:start, 0:500]
 