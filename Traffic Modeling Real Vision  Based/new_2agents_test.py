56462f8da0de9e3c927711289d5d95a676ff5b32
ishan-maheshwari <ishan_bt2k15@dtu.ac.in>
2018-10-08 20:38:32 +0530

    Testing new traffic

--- Traffic Modeling Real Vision  Based/new_2agents_test.py
+++ Traffic Modeling Real Vision  Based/new_2agents_test.py
@@ -131,7 +131,7 @@ def generate_routefile():
     <route id="r143" edges="154o 14i 010o 1o 51i"/>
     <route id="r144" edges="154o 14i 010o 4o 54i"/>
 
-    <flow id="mixed1" begin="0" end="350" number="150" route="r21" type="mixed" departLane="random" departPosLat="random"/>
+    <flow id="mixed1" begin="0" end="350" number="0" route="r21" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed2" begin="0" end="350" number="150" route="r32" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed3" begin="0" end="350" number="0" route="r31" type="mixed" departLane="random" departPosLat="random"/>
     <flow id="mixed4" begin="0" end="350" number="0" route="r41" type="mixed" departLane="random" departPosLat="random"/>
@@ -323,9 +323,9 @@ episode_time = 350
 num_vehicles = 250
 transition_time = 8
 target_update_time = 20
-q_estimator_model_left = load_model('new_2agents_model_left_5_10_5.h5')
+q_estimator_model_left = load_model('new_2agents_model_left_5_10_2_40.h5')
 #target_estimator_model_left = build_model(transition_time)
-q_estimator_model_right = load_model('new_2agents_model_right_5_10_5.h5')
+q_estimator_model_right = load_model('new_2agents_model_right_5_10_2_40.h5')
 #target_estimator_model_right = build_model(transition_time)
 replay_memory_init_size = 35
 replay_memory_size = 800
